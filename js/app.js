8bc87117a382aa9eaf742b1fc4a84cd6044fd3cd
proXDhiya <prox.dhiya@gmail.com>
2021-09-17 21:04:58 +0100

    desktop version conpleted

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// validate check function
+const validate = function(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+// selectors
+const email = document.querySelector('input[type="email"]');
+const btn = document.querySelector('input[type="submit"]');
+const errorMsg = document.querySelector('.error-msg');
+
+// click event
+btn.addEventListener(
+    'click', function() {
+        if (!validate(email.textContent)) {
+            errorMsg.classList.remove('error-none');
+            email.classList.add('error');
+        }
+    }
+);
